eaf849728c96b669c338d06b432079b739e69255
Nadav23AnT <nadav2282@gmail.com>
2023-10-25 16:57:07 +0300

    adding subnet + sg + key_pair + route + route tabl

--- main.tf
+++ main.tf
@@ -1,9 +1,72 @@
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev"
   }
+}
+
+resource "aws_subnet" "dev-subnet-01" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "dev-igw-1" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev-rt-01" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "dev-rt-default" {
+  route_table_id         = aws_route_table.dev-rt-01.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev-igw-1.id
+}
+
+resource "aws_route_table_association" "dev-public-assoc" {
+  subnet_id      = aws_subnet.dev-subnet-01.id
+  route_table_id = aws_route_table.dev-rt-01.id
+}
+
+resource "aws_security_group" "dev-sg-01" {
+  name        = "dev-sg-01"
+  description = "dev security group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # input your IP (private)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "dev_auth" {
+  key_name   = "devssh"
+  public_key = file("~/.ssh/devssh.pub")
 }
